f1b45f6e3da4371923e10e36f520ead6065de012
mistermk24 <aleksandr.seleznev24@gmail.com>
2020-08-02 22:15:03 +0200

    fixes for the issues 2

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -5,5 +5,5 @@ module TestGuru
 
   private
 
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'about', to: 'about#show'
-  get 'about/author', to: 'about#author'
+  # get 'about', to: 'about#show'
+  # get 'about/author', to: 'about#author'
 end
