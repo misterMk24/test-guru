251fd5d42b569b763b12ba431c9aff3a03b04518
misterMK24 <aleksandr.seleznev24@gmail.com>
2020-08-04 13:19:47 +0200

    Table User has been added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_222712) do
+ActiveRecord::Schema.define(version: 2020_08_04_111814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 2020_08_03_222712) do
   create_table "tests", force: :cascade do |t|
     t.string "title"
     t.integer "level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
