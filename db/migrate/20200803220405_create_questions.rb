e3d620477d01533dcebe52111df845f1aa25c11c
misterMK24 <aleksandr.seleznev24@gmail.com>
2020-08-04 12:45:24 +0200

    Association migration has been added to initial migration

--- db/migrate/20200803220405_create_questions.rb
+++ db/migrate/20200803220405_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.string :body
+      t.belongs_to :test, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200803221423_add_association.rb
+++
@@ -1,7 +0,0 @@
-class AddAssociation < ActiveRecord::Migration[6.0]
-  def change
-    change_table :questions do |t|
-      t.belongs_to :test
-    end
-  end
-end
