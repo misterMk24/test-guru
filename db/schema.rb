0ef9ede9c329d22f86bdffae84a669d4b4b00754
misterMK24 <aleksandr.seleznev24@gmail.com>
2020-08-04 13:05:23 +0200

    Model 'Answer' has been added

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_221423) do
+ActiveRecord::Schema.define(version: 2020_08_03_222712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "questions", force: :cascade do |t|
+  create_table "answers", force: :cascade do |t|
     t.string "body"
+    t.boolean "correct"
+    t.bigint "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "body"
     t.bigint "test_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
@@ -30,4 +39,6 @@ ActiveRecord::Schema.define(version: 2020_08_03_221423) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
 end
