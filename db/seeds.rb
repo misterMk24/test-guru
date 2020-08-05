a7a76d8ca5db83831e228c74d946320b4d42ef0e
misterMK24 <aleksandr.seleznev24@gmail.com>
2020-08-05 18:16:01 +0200

    seed data, add faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times { User.create!(username: Faker::Name.first_name) }
+
+["football", "basketball", "rock_music"].each { |cat| Category.create!(title: cat) }
+
+10.times { Test.create!(title: Faker::Sports::Football.player, level: rand(0..2),
+  category_id: Category.find_by(title: "football").id) }
+
+10.times { Test.create!(title: Faker::Sports::Basketball.team, level: rand(0..2),
+  category_id: Category.find_by(title: "basketball").id) }
+
+10.times { Test.create!(title: Faker::Music::RockBand.name, level: rand(0..2),
+  category_id: Category.find_by(title: "rock_music").id) }
