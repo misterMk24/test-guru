367f2e3b4696ebc7eadb3ff2352db49f6716d845
mistermk24 <aleksandr.seleznev24@gmail.com>
2020-09-15 15:43:38 +0200

    feedback_mailer_config

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -1,5 +1,5 @@
 class FeedbacksMailer < ApplicationMailer
   def user_feedback(user_email, mail_params)
-    mail to: user_email, subject: mail_params[:subject], body: mail_params[:body]
+    mail to: ENV['SMTP_USERNAME'], subject: mail_params[:subject], body: mail_params[:body]
   end
 end
