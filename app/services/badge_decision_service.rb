8b69f0690989c2f80fed39231415bbdab01a54e7
mistermk24 <aleksandr.seleznev24@gmail.com>
2020-09-22 21:38:58 +0200

    badge decision service changes

--- app/services/badge_decision_service.rb
+++ app/services/badge_decision_service.rb
@@ -3,6 +3,7 @@ class BadgeDecisionService
     @test_passage = test_passage
     @test = @test_passage.test
     @category = @test.category
+    @level = @test.level
     @user = @test_passage.user
     @badges = Badge.all
   end
@@ -16,34 +17,42 @@ class BadgeDecisionService
   private
 
   def converges?(badge)
-    if is_category?(badge) && is_level?(badge)
-      is_passages_amount?(badge) || is_first_attempt?(badge)
+    if is_category?(badge) && is_level?(badge) && is_first_attempt?(badge)
+      is_passages_amount?(badge)
     else
       return false
     end
   end
 
   def is_first_attempt?(badge)
-    (@user.tests.where(id: @test.id).count == 1) && badge.first_attempt
+    user_first_attempt = @user.tests.where(id: @test.id).count == 1
+    user_first_attempt == badge.first_attempt || user_first_attempt
   end
 
   def is_category?(badge)
-    badge_cat = badge.category
-    badge_cat == @test.category || badge_cat.nil?
+    @badge_cat = badge.category
+    @badge_cat == @category || @badge_cat.nil?
   end
 
   def is_level?(badge)
-    badge_lvl = badge.level
-    badge_lvl == @test.level || badge_lvl.nil?
+    @badge_lvl = badge.level
+    @badge_lvl == @level || @badge_lvl.nil?
   end
 
   def is_passages_amount?(badge)
-    user_tests = @user.test_passages.where(test: @test)
-    if badge.test_passages_amount.zero?
-      # means that all tests by mentioned criteria should be passed successfully
-      user_tests.count == user_tests.where(success: true) 
+    return true if badge.test_passages_amount == 1
+
+    if @badge_lvl.nil?
+      user_tests = @user.tests.where(category: @category).distinct
+      tests = Test.where(category: @category)
+    elsif @badge_cat.nil?
+      user_tests = @user.tests.where(level: @level).distinct
+      tests = Test.where(level: @level)
     else
-      badge.test_passages_amount == user_tests.where(success: true).count
+      user_tests = @user.tests.where(level: @level, category: @category).distinct
+      tests = Test.where(category: @category, level: @level)
     end
+    count = @user.test_passages.select('distinct on (test_id) *').where(test: user_tests, success: true).length    
+    badge.test_passages_amount.zero? ? tests.count == count : badge.test_passages_amount == count
   end
 end
